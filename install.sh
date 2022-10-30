57efbd06c9bae01aeef02d2f8ad8e46644c15bc6
Jade <112060601+poisk-ls@users.noreply.github.com>
2022-10-30 22:59:11 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,100 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo ""
+echo ""
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" 
+echo " Made this packages by poisk-ls🥀💛 "
+echo " add me on FB Jade Nelson Buhayan"
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+echo ""
+echo " "
+echo " "
+pkg install update -y
+echo " "
+echo " "
+echo " "
+echo ""
+echo ""
+pkg install git -y
+echo " "
+echo " "
+echo " "
+echo ""
+echo ""
+echo " Installing ruby...."
+pkg install ruby -y
+echo " "
+echo " "
+echo " "
+echo " "
+echo " Installing lolcat...."
+gem install lolcat
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo " Installing cmatrix...." | lolcat
+pkg install cmatrix -y
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo " Installing libcaca.... " | lolcat
+pkg install libcaca -y
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo " Installing telnet mapscci " | lolcat
+telnet mapscii.me
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo " Installing sl " | lolcat
+pkg install sl -y
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo " Installing cowsay " | lolcat
+pkg install cowsay -y
+echo ""
+echo ""
+echo ""
+echo ""
+echo " Installing sl " | lolcat
+pkg install sl -y
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo " Installing figlet " | lolcat
+pkg install figlet -y
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "   done Installation..." | lolcat
+echo ""
+echo ""
+echo "       ⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" | lolcat
+echo "                       follow for more "                | lolcat
+echo "                https://github.com/poisk-ls•🥀🔥 "      | lolcat
+echo "       ⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" | lolcat
+echo ""
+echo ""
+echo ""
+echo ""
+echo "      - Enjoy bhai😍" | lolcat
+echo ""
+exit
